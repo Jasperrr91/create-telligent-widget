c11148e1b3f0b68713a02213f269176576006238
Jasper <jasper@vanderstoop.nl>
2020-02-06 16:01:16 +0100

    :tada: Simple CLI Application

---
+++ src/create.js
@@ -0,0 +1,6 @@
+import fs from 'fs';
+
+// fs.mkdirSync('widget');
+export const createWidget = (name) => {
+  console.log(name);
+};
---
+++ src/generator/index.js
@@ -0,0 +1,6 @@
+import fs from 'fs';
+
+// fs.mkdirSync('widget');
+export default function createWidget(name) {
+  console.log(name);
+};
